29c9d3091f4c03f8bf125e82850bfc6ebc2e2d65
Xiaokang <dreamytzk@outlook.com>
2020-08-25 19:03:44 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require("gulp");
+const minifycss = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const htmlmin = require("gulp-htmlmin");
+const cssnano = require("gulp-cssnano");
+const htmlclean = require("gulp-htmlclean");
+const del = require("del");
+const babel = require("gulp-babel");
+const autoprefixer = require("gulp-autoprefixer");
+const connect = require("gulp-connect");
+const pug = require("gulp-pug");
+const sass = require("gulp-sass");
+sass.compiler = require("node-sass");
+
+const config = require("./config.json");
+
+gulp.task("clean", function () {
+	return del(["./dist/css/", "./dist/js/"]);
+});
+
+gulp.task("css", function () {
+	return gulp
+		.src("./src/css/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(minifycss({ compatibility: "ie8" }))
+		.pipe(autoprefixer({ overrideBrowserslist: ["last 2 version"] }))
+		.pipe(cssnano({ reduceIdents: false }))
+		.pipe(gulp.dest("./dist/css"));
+});
+
+gulp.task("html", function () {
+	return gulp
+		.src("./dist/*.html")
+		.pipe(htmlclean())
+		.pipe(htmlmin())
+		.pipe(gulp.dest("./dist"));
+});
+
+gulp.task("js", function () {
+	return gulp
+		.src("./src/js/*.js")
+		.pipe(babel({ presets: ["@babel/preset-env"] }))
+		.pipe(uglify())
+		.pipe(gulp.dest("./dist/js"));
+});
+
+gulp.task("pug", function () {
+	return gulp
+		.src("./src/*.pug")
+		.pipe(pug({ data: config }))
+		.pipe(gulp.dest("./dist"));
+});
+
+gulp.task("assets", function () {
+	return gulp.src(["./src/assets/**/*"]).pipe(gulp.dest("./dist/assets"));
+});
+
+gulp.task("build", gulp.series("clean", "assets", "pug", "css", "js", "html"));
+gulp.task("default", gulp.series("build"));
+
+gulp.task("watch", function () {
+	gulp.watch("./src/components/*.pug", gulp.parallel("pug"));
+	gulp.watch("./src/pages/*.pug", gulp.parallel("pug"));
+	gulp.watch("./src/*.pug", gulp.parallel("pug"));
+	gulp.watch("./src/css/**/*.scss", gulp.parallel(["css"]));
+	gulp.watch("./src/js/*.js", gulp.parallel(["js"]));
+	connect.server({
+		root: "dist",
+		livereload: true,
+		port: 8080,
+	});
+});
