e3bfb0f875941b70efcc0b34624ebdc4c102a9d9
CCKNBC <ccknbc@qq.com>
2021-12-12 18:06:25 +0800

    🐛 更新天气 API

--- src/js/about.js
+++ src/js/about.js
@@ -47,18 +47,19 @@
 		addLine(`<span class="time">${time.getFullYear()}-${time.getMonth()+1}-${time.getDate()}</span> <span class="info">Address:</span><span> Hubei,China</span>`)
 		addLine(`<span class="time">${time.getFullYear()}-${time.getMonth()+1}-${time.getDate()}</span> <span class="info">Email:</span><span> cc@ccknbc.cc</span>`)
 		addLine(`<span class="success">Done</span><span> Myself introduction is over!</span>`)
-		addLine(`<span class="info">Tip</span><span> The weather in your city is as follows</span>`)
-		addLine('$ weather')
+		addLine(`<span class="info">Tip</span><span> The weather in CC's city is as follows</span>`)
+		addLine('$ weathercc')
+		commands.weathercc();
+		commands.aircc();
 		commands.weather()
 	}
-	
 	var commands = {
 		help: function () {
 			addLine("Available command list:");
-			// addLine("<span class='success'>blog</span>   open CC's blog");
+			addLine("<span class='success'>blog</span>   open CC's blog");
 			// addLine("<span class='success'>skill</span>   see CC's skill");
-			addLine("<span class='success'>me</span>   About CC");
-			// addLine("<span class='success'>source</span>   see my site source");
+			// addLine("<span class='success'>me</span>   About CC");
+			addLine("<span class='success'>source</span>   see my site source");
 			addLine("<span class='success'>chat</span>   Chat With CC Online");
 		},
 		blog: function () {
@@ -81,18 +82,46 @@
 		},
 		weather:function (){
 		return 	$.ajax({
-				url:'https://v1.alapi.cn/api/tianqi/now',
+				url:'https://v2.alapi.cn/api/tianqi?token=BnqWzZxXlU9oxo6e',
 				success:function (data){
-					addLine(`*********************`)
-					addLine(`<span class='success'>City</span>  ${data.data.country} ${data.data.city}`);
-					addLine(`<span class='success'>Weater</span>  ${data.data.wea}`);
-					addLine(`<span class='success'>Win</span>  ${data.data.win}`);
-					addLine(`<span class='success'>Tip</span>  ${data.data.air_tips}`);
-					addLine(`*********************`);
-					addLine(`<span class="info">Tip</span><span> Input 'help' to see more!</span>`)
+					addLine(`<span class="info">Tip</span><span> The weather in your city is as follows</span>`);
+					addLine('$ weather')
+					addLine(`<span class='success'>City</span>  ${data.data.province} ${data.data.city}`);
+					addLine(`<span class='success'>Weather</span>  ${data.data.weather} ${data.data.wind}${data.data.wind_speed} 当前温度 ${data.data.temp} ℃ 最低温度 ${data.data.min_temp} ℃ 最高温度${data.data.max_temp} ℃，`);
+					addLine(`<span class='success'>Air</span>  ${data.data.aqi.air_tips}`);
+					addLine(`<span class='success'>Sun</span>  日出时间：${data.data.sunrise} 日落时间：${data.data.sunset}`);
+					addLine(`<span class="info">Tip</span><span> Input 'help' to see more!</span>`);
 				}
 			})
 		},
+		weathercc:function (){
+		return 	$.ajax({
+				url:'https://devapi.qweather.com/v7/weather/now?key=081c90ab04874a0fb4cf9ecd269b10b8&location=101190407',
+				success:function (data){
+					addLine(`<span class='success'>Weather</span>  ${data.now.text} ${data.now.windDir}(${data.now.wind360}°)${data.now.windScale}级 ${data.now.windSpeed}公里/小时`);
+					addLine(`<span class='success'>Temp</span>  当前温度：${data.now.temp}℃ 体感温度：${data.now.feelsLike}℃`);
+					addLine(`<span class='success'>Other</span>  相对湿度：${data.now.humidity} 大气压强：${data.now.pressure}hPa 能见度：${data.now.vis}km 云量：${data.now.cloud}，露点温度：${data.now.dew}℃`);
+				}
+			})
+		},
+		aircc:function (){
+		return 	$.ajax({
+				url:'https://devapi.qweather.com/v7/air/now?key=081c90ab04874a0fb4cf9ecd269b10b8&location=101190407',
+				success:function (data){
+					addLine(`<span class='success'>Air</span>  空气质量指数：${data.now.aqi} 等级：${data.now.level} 级别：${data.now.category} 主要污染物：${data.now.primary}`);
+					addLine(`<span class='success'>Pollution</span>  PM10：${data.now.pm10} PM2.5：${data.now.pm2p5} 二氧化氮：${data.now.no2} 二氧化硫：${data.now.so2} 一氧化碳：${data.now.co} 臭氧：${data.now.o3}`);
+				}
+			})
+		},
+		// lifecc:function (){
+		// return 	$.ajax({
+		// 		url:'https://devapi.qweather.com/v7/indices/1d?type=0&location=101190407&key=081c90ab04874a0fb4cf9ecd269b10b8',
+		// 		success:function (data){
+		// 			addLine(`<span class='success'>Life</span>  ${data.daily.name}${data.daily.level}${data.daily.catagory}${data.daily.text}`);
+		// 			addLine(`*********************`);
+		// 		}
+		// 	})
+		// },
 		open: function (command) {
 			if (command.length >= 2) {
 				window.open(command[1], '_blank').location;
